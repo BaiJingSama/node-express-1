6c4b444c2bd7bdb3869e601cf4e5ced42658b564
gyc <gouyuchen@zhiyuanhui.com>
2023-12-08 13:53:32 +0800

    hello world

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/sew', (request, response) => {
+  response.send('宋二娃123')
+  // 发送响应
+})
+
+app.listen(port, () => {
+  console.log(`宋文倩正在竖着耳朵监听 ${port} 端口`)
+})
